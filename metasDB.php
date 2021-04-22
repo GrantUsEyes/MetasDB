61b56139abb2d77d56f56d3377f189938c71bd86
human-1914 <49424610+human-1914@users.noreply.github.com>
2021-04-22 17:44:54 -0500

    Add files via upload
    metasDB website php file.

---
+++ metasDB.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+	<header><h1>Welcome to MetasDB</h1>
+	<h4>Cancer-associated fibroblasts are an area of specific interest in the realm of cancer research. These fibroblasts have been found to speed up (and occasionally slow down) the spread of cancer. Understanding the effect that CAFs have on cancer metastasis may be crucial to develop anti-metastasis drugs to combat the spread of a tumor within a patient. To better investigate CAFs' role in tumor metastasis, we can use computational biology to simulate the interactions between CAFs and tumor cells within the tumor microenvironment. The intention of MetasDB is to store and analyze the results of the simulations in a way that are queryable to the user. </h4></header>
+	
+	<body>
+		<form method="post" action"<?php echo $_SERVER['PHP_SELF'];?>">
+	</body>
+</html>
+
